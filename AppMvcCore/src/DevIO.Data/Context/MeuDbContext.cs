d502089e5f0b8d8a1550d0e0bf0e7cd847cceb86
MarcMelo <marcilio_sergio@hotmail.com>
2020-06-30 16:29:26 +0100

    AspNetCore App

---
+++ AppMvcCore/src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,37 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DevIO.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                    .Where(p => p.ClrType == typeof(string))))
+                property.Relational().ColumnType = "varchar(100)";
+
+            //Registra cada "Mapping" de uma única vez
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            //Impedindo que um Dado que está sendo deletada do banco também remova os filhos
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
